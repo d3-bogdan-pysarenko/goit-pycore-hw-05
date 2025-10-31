c4440e1be155514e2268cf640f12ceefebd8b60e
Bogdan Pysarenko <bogdan.pysarenko@door3.com>
2025-10-31 09:55:32 -0400

    task_02

---
+++ task_02.py
@@ -0,0 +1,25 @@
+import re
+from typing import Callable, Generator
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+
+    # Finding mumbers that are separated with spaces
+    reg_ex = r'\b\d+(?:\.\d+)?\b'
+    numbers = re.findall(reg_ex, text)
+    for num in numbers:
+        yield float(num)
+
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    return sum(func(text))
+
+
+# Check time
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
+
+# text2 = "Our income is $1200.50, but our expenses are $300.25 so it gives us pure $900.25. plus my aunt's comission in $25.19"
+# result = sum_profit(text2, generator_numbers)
+
+# print(result)
