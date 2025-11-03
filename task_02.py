56f18b2d87c173f24c0c98b71a68c5461e2f5785
Bogdan Pysarenko <bogdan.pysarenko@door3.com>
2025-11-03 03:47:43 -0500

    task2 upd

--- task_02.py
+++ task_02.py
@@ -15,11 +15,13 @@ def sum_profit(text: str, my_function: Callable):
 
 
 # Check time
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, " \
+    "доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
 
-text2 = "Our income is $1200.50, but our expenses are $300.25 so it gives us pure $900.25. plus my aunt's comission in $25.19"
+text2 = "Our income is $1200.50, but our expenses are $300.25 so it gives us pure $900.25. plus my aunt's comission in $25.19" \
+    " but function is just summing it up altogather"
 result = sum_profit(text2, generator_numbers)
 
 print(result)
